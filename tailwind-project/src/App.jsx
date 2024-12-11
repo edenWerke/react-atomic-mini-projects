3a9dbc5d0823243b8de28aa99ce8ebb5a9712590
edenWerke <edenw6292@gmail.com>
2024-12-11 11:33:05 +0300

    initial commitof tailwind

---
+++ tailwind-project/src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <section className="items-center justify-center h-screen p-12 m-12 text-3xl text-center bg-twitter-blue md:p-14 lg:p-16 xl:p-20">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil, quas
+          enim voluptate molestias sapiente ducimus ab? Quis quos quo,
+          doloribus, cupiditate dolore maiores illum obcaecati, perspiciatis
+          odit deleniti officia dolor?{" "}
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default App;
