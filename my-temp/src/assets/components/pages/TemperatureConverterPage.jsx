a19189c4efa6228c6ef1ea06e132868385557f75
edenWerke <edenw6292@gmail.com>
2024-12-12 22:42:44 +0300

    Atom-Design page

---
+++ my-temp/src/assets/components/pages/TemperatureConverterPage.jsx
@@ -0,0 +1,7 @@
+// /pages/TemperatureConverterPage.js
+// import React from 'react';
+import MainTemplate from "../templates/MainTemplate";
+
+const TemperatureConverterPage = () => <MainTemplate />;
+
+export default TemperatureConverterPage;
